a680254942619463bc92320f512724fff31a26cb
Jose Abel Castellanos Joo <jcastellanos34@gmail.com>
2014-06-10 17:42:05 -0500

    Ahora Signal/Collect tiene enfoque funcional

--- Prueba.scala
+++ Prueba.scala
@@ -2,14 +2,14 @@ import com.signalcollect._
 
 object SSSP extends App {
   val graph = GraphBuilder.build
-    graph.addVertex(new Location(1, Some(0)))
-    graph.addVertex(new Location(2))
-    graph.addVertex(new Location(3))
-    graph.addVertex(new Location(4))
-    graph.addEdge(1, new Path(2, 4.2))
-    graph.addEdge(2, new Path(3, 3.12))
+    graph addVertex(new Location(1, Some(0)))
+    graph addVertex(new Location(2))
+    graph addVertex(new Location(3))
+    graph addVertex(new Location(4))
+    graph addEdge(1, new Path(2, 4.2))
+    graph addEdge(2, new Path(3, 3.12))
     graph.execute
-    graph.foreachVertex(println(_))
+    graph foreachVertex (println(_))
     graph.shutdown    
 }
 
@@ -27,8 +27,10 @@ class Location(id: Int, initialState: Option[Double] = None)
     val sourceState = source.state.get
 
   	val newState = state match {  	
-   	 	case None => Some(signalState)
-    	case Some(currentShortestPath) => Some(math.min(currentShortestPath, signalState))
+   	 	case None => 
+   	 		Some(signalState)
+    	case Some(currentShortestPath) => 
+    		Some(math.min(currentShortestPath, signalState))
   	}
 
   	newState
