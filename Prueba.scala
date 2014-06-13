8dc589c4d93d44dae43e16ad26aff4c817ef215d
Jose Abel Castellanos Joo <jcastellanos34@gmail.com>
2014-06-13 02:16:44 -0500

    Computes the Max Flow. The only problem is that I cannot shutdown the graph

--- Prueba.scala
+++ Prueba.scala
@@ -1,71 +1,172 @@
 import com.signalcollect._
 
-object SSSP extends App {
+object SSSP extends App {	
+
+	def Printing: Unit = {		
+		for(i <- 0 until Vertex.length)
+			println(Vertex(i))
+
+		for(i <- 0 until Edges.length)
+			println(Edges(i)._1 + " " + Edges(i)._2)		
+	}
 	
-	def Printing: Unit = {
-		// Printing **********************************
-		for(i <- 0 until BFSVertex.length)
-			println(BFSVertex(i))
-
-		for(i <- 0 until BFSEdges.length)
-			println(BFSEdges(i)._1 + " " + BFSEdges(i)._2)
-		// Printing **********************************
-	}
-
-	def ForEach(data: List[Int], f: Int => Unit): Unit = data match {
-		case x :: xs => 
-			f(x)
-			ForEach(xs, f)
-		case Nil => 
-			()
-	}
-
-	// Graph Data **************************************************************************************
+	// Graph Data **********************************************************************************
+	var max_flow: Double = 0.0
 	val s = 0
-	val t = 3
-	var matrixGraph = Array(Array(0, 1, 3, 0), Array(1, 0, 1, 5), Array(3, 1, 0, 3), Array(0, 5, 3, 0))
-	var parent = for{p <- (0 until matrixGraph.length).toArray} yield -1
-
-	var BFSVertex: Array[BFSVertex] = ( for {p <- 0 until matrixGraph.length} yield 
+	val t = 5 
+	var matrixGraph: Array[Array[Double]] = Array(Array(0, 16, 13, 0, 0, 0),
+  																							Array(0, 0, 10, 12, 0, 0),
+																								Array(0, 4, 0, 0, 14, 0),
+																								Array(0, 0, 9, 0, 0, 20),
+																								Array(0, 0, 0, 7, 0, 4),
+																								Array(0, 0, 0, 0, 0, 0))
+	var parent = for{p <- (0 until matrixGraph.length).toArray} yield -1	
+	// Graph Data **********************************************************************************
+	
+	// Import Graph ********************************************************************************
+	var Vertex: Array[BFSVertex] = ( for {p <- 0 until matrixGraph.length} yield 
 																		if(p == s)  (new BFSVertex(p, Some(0), parent(p))) 
 																		else		    (new BFSVertex(p, None, parent(p))) ).toArray
 
-	var BFSEdges: Array[(Int, BFSEdge)] = ( for {i <- 0 until matrixGraph.length; 
+	var Edges: Array[(Int, BFSEdge)] = ( for {i <- 0 until matrixGraph.length; 
 																				j <- 0 until matrixGraph.length; 
 																				if(matrixGraph(i)(j) > 0)} yield
 																					(i, new BFSEdge(j, matrixGraph(i)(j))) ).toArray
-	// Graph Data **************************************************************************************
+	var graph = GraphBuilder.build
 	
-	val graph = GraphBuilder.build
-	// Import Graph ******************************
-  for(i <- 0 until BFSVertex.length)
-  	graph addVertex (BFSVertex(i))		
+  for(i <- 0 until Vertex.length)
+  	graph addVertex (Vertex(i))		
 
-	for(i <- 0 until BFSEdges.length)
-		graph addEdge (BFSEdges(i)._1, BFSEdges(i)._2)
-	// Import Graph ******************************		
-	
-	//Printing
+	for(i <- 0 until Edges.length)
+		graph addEdge (Edges(i)._1, Edges(i)._2)
+	// Import Graph ********************************************************************************
 
 	// Signal/Collect Computation ****************	
-  graph.execute
-  graph.shutdown
+  graph.execute  
   // Signal/Collect Computation ****************
 
-  Printing
-  /*
-  println("Lol " + BFSEdges(0)._1 + " " + BFSEdges(0)._2)
-  BFSEdges(0)._2.w = 121.23
-	println("Lol " + BFSEdges(0)._1 + " " + BFSEdges(0)._2)
-	*/
+ 	// Update parent ********************
+ 	for(i <- 0 until Vertex.length)
+  	parent(i) = Vertex(i).parent
+	// Update parent ********************
+  
+	// Get Min Path Flow from Node T to Node S ******************	
+	var son = t
+	var minPathFlow = (Int.MaxValue).toDouble	
+	var i = parent(son)
 
-	// TraceBack from T Node to S Node
-	var i = t
-	while(parent(i) > 0){
-		println	(parent(i))
+	while(parent(i) >= 0){
+		if(minPathFlow > (Vertex(i).outgoingEdges(son)).weight)
+			minPathFlow = (Vertex(i).outgoingEdges(son)).weight
+		son = i
 		i = parent(i)
 	}
 
+	if(minPathFlow > (Vertex(i).outgoingEdges(son)).weight)
+			minPathFlow = (Vertex(i).outgoingEdges(son)).weight	
+	max_flow += minPathFlow
+	// Get Min Path Flow from Node T to Node S ******************
+
+	// Update Graph *********************************************
+	i = t
+	var j = parent(i)
+
+	while(parent(i) >= 0){		
+		j = parent(i)
+		matrixGraph(j)(i) -= minPathFlow;
+		matrixGraph(i)(j) += minPathFlow;
+		i = parent(i)
+	}
+	
+	Vertex = ( for {p <- 0 until matrixGraph.length} yield 
+							if(p == s)  (new BFSVertex(p, Some(0), parent(p))) 
+							else		    (new BFSVertex(p, None, parent(p))) ).toArray
+	Edges = ( for {i <- 0 until matrixGraph.length; 
+								j <- 0 until matrixGraph.length; 
+								if(matrixGraph(i)(j) > 0)} yield
+									(i, new BFSEdge(j, matrixGraph(i)(j))) ).toArray
+	
+	graph = GraphBuilder.build
+
+	for(i <- 0 until Vertex.length)
+  	graph addVertex (Vertex(i))		
+
+	for(i <- 0 until Edges.length)
+		graph addEdge (Edges(i)._1, Edges(i)._2)
+	// Update Graph *********************************************
+
+	// Repeat Again Until No Augmented Path exists ****************************	
+	// Signal/Collect Computation ****************	
+  graph.execute  
+  // Signal/Collect Computation ****************
+ 	
+ 	// Update parent ********************
+ 	for(i <- 0 until Vertex.length)
+  	parent(i) = Vertex(i).parent
+	// Update parent ********************
+	
+	while(Vertex(t).state != None){
+		// Get Min Path Flow from Node T to Node S ******************	
+		son = t
+		minPathFlow = (Int.MaxValue).toDouble	
+		i = parent(son)
+
+		while(parent(i) >= 0){
+			if(minPathFlow > (Vertex(i).outgoingEdges(son)).weight)
+				minPathFlow = (Vertex(i).outgoingEdges(son)).weight
+			son = i
+			i = parent(i)
+		}
+
+		if(minPathFlow > (Vertex(i).outgoingEdges(son)).weight)
+				minPathFlow = (Vertex(i).outgoingEdges(son)).weight			
+		max_flow += minPathFlow
+		// Get Min Path Flow from Node T to Node S ******************
+
+		// Update Graph *********************************************
+		i = t
+		j = parent(i)
+
+		while(parent(i) >= 0){		
+			j = parent(i)
+			matrixGraph(j)(i) -= minPathFlow;
+			matrixGraph(i)(j) += minPathFlow;
+			i = parent(i)
+		}
+
+		Vertex = ( for {p <- 0 until matrixGraph.length} yield 
+							if(p == s)  (new BFSVertex(p, Some(0), parent(p))) 
+							else		    (new BFSVertex(p, None, parent(p))) ).toArray
+		Edges = ( for {i <- 0 until matrixGraph.length; 
+								j <- 0 until matrixGraph.length; 
+								if(matrixGraph(i)(j) > 0)} yield
+									(i, new BFSEdge(j, matrixGraph(i)(j))) ).toArray
+
+		graph = GraphBuilder.build
+		
+		for(i <- 0 until Vertex.length)
+	  	graph addVertex (Vertex(i))		
+
+		for(i <- 0 until Edges.length)
+			graph addEdge (Edges(i)._1, Edges(i)._2)
+		// Update Graph ****************************************************
+				
+		// Signal/Collect Computation ****************	
+	  graph.execute  
+	  // Signal/Collect Computation ****************
+	 	
+	 	// Update parent *****************************
+	 	for(i <- 0 until Vertex.length)
+	  	parent(i) = Vertex(i).parent
+	 	// Update parent *****************************
+
+	}
+	// Repeat Again Until No Augmented Path existis ****************************
+
+	println("El Max Flow es: " + max_flow)
+		
+	graph.shutdown
+	
 }
 
 class BFSVertex(id: Int, initialState: Option[Double] = None, var parent: Int)
@@ -80,7 +181,7 @@ class BFSVertex(id: Int, initialState: Option[Double] = None, var parent: Int)
 		val source = signal._1
   	val signalState = signal._2	
 
-  	val newState = state match {  	
+  	val newState = state match {  		
    	 	case None => 
    	 		parent = source.id
    	 		Some(signalState)
@@ -89,7 +190,7 @@ class BFSVertex(id: Int, initialState: Option[Double] = None, var parent: Int)
     			parent = source.id
     		Some(math.min(currentShortestBFSEdge, signalState))
   	}
-  
+
   	newState
   
   }
